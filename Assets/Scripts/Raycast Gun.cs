bc648c5628e604622c6b042f5fbbbd244695ccb5
AzureAD\AlbertVelezMuga <albert.velez@enti.cat>
2024-11-19 17:56:40 +0100

    save 12

---
+++ Assets/Scripts/Raycast Gun.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastGun : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Transform laserOrigin;
+    public float GunRange = 50f;
+    public float fireRate = 0.2f;
+    public float laserDuration = 0.05f;
+
+    LineRenderer laserLine;
+    float fireTimer;
+    // Start is called before the first frame update
+
+     void Awake()
+    {
+        laserLine = GetComponent<LineRenderer>();
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireTimer += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fireTimer = 0;
+            //laserLine.SetPosition(0, laserOrigin.position);
+            RaycastHit hit;
+            if(Physics.Raycast(laserOrigin.position, laserOrigin.forward, out hit, GunRange))
+            {
+                //laserLine.SetPosition(1, hit.point);
+                Destroy(hit.transform.gameObject);
+            }
+            else
+            {
+                //laserLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * GunRange));
+            }
+            StartCoroutine(ShootLaser());
+        }
+    }
+    IEnumerator ShootLaser()
+    {
+        //laserLine.enabled = true;
+        yield return new  WaitForSeconds(laserDuration) ;
+        //laserLine.enabled = false;
+    }
+}
